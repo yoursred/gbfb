c039d22e743f661a6d1fe6ab5a0b713eda633f69
yoursred <yoursred@yoursred.com>
2024-11-18 08:49:24 +0100

    RGBA

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from PIL import Image
 
 COLORS = [0, 85, 170, 255]
 
-screen = Image.new('L', (160, 144), 255)
+screen = Image.new('RGBA', (160, 144), 255)
 
 
 with open('fb.bin', 'rb') as fp:
@@ -11,6 +11,6 @@ with open('fb.bin', 'rb') as fp:
 
 for x in range(160):
     for y in range(144):
-        screen.putpixel((x, y), COLORS[fb[y * 160 + x]])
+        screen.putpixel((x, y), (fb[(y * 160 + x) * 4], fb[(y * 160 + x) * 4 + 1], fb[(y * 160 + x) * 4 + 2], fb[(y * 160 + x) * 4 + 3]))
 
 screen.save('screen.png')
